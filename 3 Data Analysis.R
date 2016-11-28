18892ea0bb05634a57a4c2f174f01c67154c5fff
Bruns <Hendrik.Bruns@wiso.uni-hamburg.de>
2016-11-28 18:55:00 +0100

    Including bar plots with error bars (SE or CI) as alternative to boxplots

--- 3 Data Analysis.R
+++ 3 Data Analysis.R
@@ -7,7 +7,8 @@ load("2 results_cleaned.RData")
 
 library(psych)
 library(dunn.test)
-library(ggplot2)
+library(ggplot2) # for visualization
+library(dplyr) # for data manipulation
 # Data Analysis
 
 # Manuscript ----
@@ -43,6 +44,58 @@ ggplot(data = df, aes(x = Treatment, y = Contribution)) +
         axis.title.x = element_text(size = 26),
         axis.title.y = element_text(size = 26),
         axis.text = element_text(vjust=.5, size=16))
+
+# Figure 1 (alternative: Barplot with error bars/ 95CIs) ----
+# below function calculates necessary components of bar plots
+summarySE <- function(data=NULL, measurevar, groupvars=NULL, na.rm=FALSE,
+                      conf.interval=.95, .drop=TRUE) {
+  library(plyr)
+  
+  # New version of length which can handle NA's: if na.rm==T, don't count them
+  length2 <- function (x, na.rm=FALSE) {
+    if (na.rm) sum(!is.na(x))
+    else       length(x)
+  }
+  
+  # This does the summary. For each group's data frame, return a vector with
+  # N, mean, and sd
+  datac <- ddply(data, groupvars, .drop=.drop,
+                 .fun = function(xx, col) {
+                   c(N    = length2(xx[[col]], na.rm=na.rm),
+                     mean = mean   (xx[[col]], na.rm=na.rm),
+                     sd   = sd     (xx[[col]], na.rm=na.rm)
+                   )
+                 },
+                 measurevar
+  )
+  
+  # Rename the "mean" column    
+  datac <- rename(datac, c("mean" = measurevar))
+  
+  datac$se <- datac$sd / sqrt(datac$N)  # Calculate standard error of the mean
+  
+  # Confidence interval multiplier for standard error
+  # Calculate t-statistic for confidence interval: 
+  # e.g., if conf.interval is .95, use .975 (above/below), and use df=N-1
+  ciMult <- qt(conf.interval/2 + .5, datac$N-1)
+  datac$ci <- datac$se * ciMult
+  
+  return(datac)
+}
+
+bar <- summarySE(df, measurevar = "Contribution", groupvars = "Treatment")
+
+ggplot(data = bar, aes(x = Treatment, y = Contribution)) +
+  geom_bar(fill = "grey", color = "black", stat = "identity", width = 0.5) +
+  geom_errorbar(aes(ymin = (Contribution - ci), ymax = (Contribution + ci)), width = 0.25) +
+  scale_y_continuous(breaks = c(0, 1, 2, 3, 4, 5), limits = c(0,5)) +
+  labs(x = "Experimental group", y = "Contribution [Credits]") +
+  theme(legend.position="none",
+        plot.title = element_text(angle = 45, vjust=.5,size = 10),
+        axis.title.x = element_text(size = 26),
+        axis.title.y = element_text(size = 26),
+        axis.text = element_text(vjust=.5, size=16))
+
 
 # Shapiro Wilk normality test ----
 shapiro.test(df$Contribution)
